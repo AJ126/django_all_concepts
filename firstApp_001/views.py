c65efaa3965fb728b1aeccba79573140e3e16d43
AJ126 <akash.jagtap866@gmail.com>
2026-09-05 17:39:50 +0530

    first app changes

--- firstApp_001/views.py
+++ firstApp_001/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+def display(request):
+    now = datetime.datetime.now()
+    html = f"<html><body><h1>Hello, World!</h1><p>The current time is: {now}</p></body></html>"
+    return HttpResponse(html)
